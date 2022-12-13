aaff6853d5246c35c164634798fed65f1ccc1d74
nichu <nichubg3.1@gmail.com>
2022-12-13 23:44:10 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import Vendor from './Vendor';
+import Purchase from './Purchase';
+import Dispatch from './Dispatch';
+import Report from './Report';
+import Category from './Category';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Vendor/>
+      {/* <Purchase/>
+      <Dispatch/>
+      <Report/>
+      <Category/> */}
     </div>
   );
 }
---
+++ src/Dispatch.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Dispatch() {
+  return (
+    <div>Dispatch</div>
+  )
+}
+
+export default Dispatch

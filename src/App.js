bd1316c12bab98f30bd769493fe1442d5cbfc9f7
Madhumitha-16 <madhu.br03@gmail.com>
2023-01-03 16:43:29 +0530

    Login&purchase commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-import logo from './logo.svg';
 import './App.css';
 import Vendor from './Vendor';
-import Purchase from './Purchase';
+import Purchase from './Components/purchaseComponent/purchase';
 import Dispatch from './Dispatch';
 import Report from './Report';
 import Category from './Category';
@@ -9,16 +8,17 @@ import { ProSidebarProvider } from 'react-pro-sidebar';
 import Slidebar from './Slidebar';
 import { BrowserRouter as Router,Routes,Route} from 'react-router-dom';
 import Stock from './Stock';
-
+import Login from './Components/loginComponent/login';
 function App() {
   return (
     <div>
      <Router>
       <Routes>
+        <Route path='/' element={<Login/>}/>
         <Route path='/dis' element={<Dispatch/>}/>
         <Route path='/rep' element={<Report/>}/>
         <Route path='/purs' element={<Purchase/>}/>
-        <Route path='/' element={<Category/>}/>
+        <Route path='/category' element={<Category/>}/>
         <Route path='/ven' element={<Vendor/>}/>
         <Route path='/stock' element={<Stock/>}/>
       </Routes>
